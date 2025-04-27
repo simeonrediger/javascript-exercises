37391e9066c209b14a775b4b15136f875fca1d02
Simeon Rediger <simeonrediger@gmail.com>
2025-04-26 22:16:30 -0400

    Implement function to sum all numbers in range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(n, m) {
+    const inputsAreNonNegative = n >= 0 && m >= 0;
+    const inputsAreIntegers = n === parseInt(n) && m === parseInt(m);
 
+    if (!inputsAreNonNegative || !inputsAreIntegers) {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+    for (let i = Math.min(n, m); i <= Math.max(n, m); i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
