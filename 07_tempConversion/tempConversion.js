f995e849738cb754cc0e7c30857b7726ef7cf024
Simeon Rediger <simeonrediger@gmail.com>
2025-04-26 22:39:15 -0400

    Implement Fahrenheit and Celsius converters

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  return Math.round(10 * (tempInF - 32) * 5 / 9) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  return Math.round(10 * (tempInC * 9 / 5 + 32)) / 10;
 };
 
 // Do not edit below this line
