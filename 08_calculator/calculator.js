e76090b1f0577e34d2f11d274f2178f221a03efb
Simeon Rediger <simeonrediger@gmail.com>
2025-05-07 02:53:40 -0400

    Implement calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(operand1, operand2) {
+	return operand1 + operand2;
 };
 
-const subtract = function() {
-	
+const subtract = function(operand1, operand2) {
+	return operand1 - operand2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(number => sum += number);
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  array.forEach(number => product *= number);
+	return product;
 };
 
-const power = function() {
-	
+const power = function(operand1, operand2) {
+	return operand1 ** operand2;
 };
 
-const factorial = function() {
-	
+const factorial = function(operand) {
+  let product = 1;
+
+	for (let i = 1; i <= operand; i++) {
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
