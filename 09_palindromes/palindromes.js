814e1a9da8625a9c52b7be44e56d268a204b7aca
Simeon Rediger <simeonrediger@gmail.com>
2025-05-07 03:35:58 -0400

    Implement palindrome checker

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const letters = Array.from(string)
+        .filter(letter => /[a-zA-Z0-9]/.test(letter))
+        .join('')
+        .toLowerCase();
 
+    const lettersReversed = Array.from(letters)
+        .reverse()
+        .join('')
+        .toLowerCase();
+
+    return letters === lettersReversed;
 };
 
 // Do not edit below this line
