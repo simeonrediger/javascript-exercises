171568b85f849a72389fb308e289d9b0cd957e44
Simeon Rediger <simeonrediger@gmail.com>
2025-05-07 04:08:12 -0400

    Implement fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n < 0 || n != Math.floor(n)) {
+        return 'OOPS';
+    }
+
+    let currentNum = 0;
+    let prevNum = 0;
+    let twicePrevNum = 0;
+
+    for (let i = 1; i <= n; i++) {
+        currentNum = twicePrevNum + prevNum || 1;
+        twicePrevNum = prevNum;
+        prevNum = currentNum;
+    }
+
+    return currentNum;
 };
 
 // Do not edit below this line
